90d008a495d0d20a81db9295441151a3d772bdc2
miuye256 <fallioss13@outlook.jp>
2025-01-17 07:51:48 +0900

    h

--- src/data/item.ts
+++ src/data/item.ts
@@ -1,32 +1,100 @@
 import { Item } from "../types/item";
 
 export const items: Item[] = [
-    {
-        id: "1",
-        name: "ミネラルウォーター",
-        description: "すっきりとした富士山の水",
-        price: 100,
-        imageUrl: "/img/water.png"
-    },
-    {
-        id: "2",
-        name: "コーヒー",
-        description: "香ばしいコーヒー",
-        price: 150,
-        imageUrl: "/img/coffee.png"
-    },
-    {
-        id: "3",
-        name: "コーラ",
-        description: "甘くて美味しいコーラ",
-        price: 200,
-        imageUrl: "/img/cola.png"
-    },
-    {
-        id: "4",
-        name: "お茶",
-        description: "おいしいお茶",
-        price: 120,
-        imageUrl: "/img/tea.png"
-    }
-]
+  {
+    id: "jasmine",
+    name: "ジャスミン茶",
+    description:
+      "最高級茶葉「銀毫（ぎんごう）」を30%使用したジャスミン茶です。ジャスミンの爽やかな香りが特徴です",
+    price: 120,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/jasmine.webp",
+  },
+  {
+    id: "dodecamine",
+    name: "ドデカミン",
+    description:
+      "爽快な炭酸と豊かなミネラルが特徴のスポーツドリンク。12種類の元気成分と強炭酸の刺激で疲れた気分を吹き飛ばし気分が弾けるエナジー炭酸飲料。",
+    price: 130,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/dodecamine.webp",
+  },
+  {
+    id: "coca-cola",
+    name: "コカ・コーラ",
+    description:
+      "世界中で愛される定番炭酸飲料。深みのあるコーラ風味とシュワシュワの炭酸が絶妙にマッチし、どんな食事にもよく合います。リフレッシュしたい時にぴったりの一杯です。",
+    price: 150,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/coca-cola.webp",
+  },
+  {
+    id: "mascotto",
+    name: "マスカットウォーター",
+    description:
+      "すっきりとした果実感が感じられる、低カロリーなマスカットウォーター。午後の一休みにぴったりな爽やかな味わいです。",
+    price: 160,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/mascotto.webp",
+  },
+  {
+    id: "calpis-water",
+    name: "カルピスウォーター",
+    description:
+      "すっきり爽やかな味わい、純水でおいしく作ったカルピスです。乳酸菌と酵母、発酵という自然製法が生みだす甘ずっぱいおいしさ。子供から大人まで幅広く楽しめます。",
+    price: 130,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/calpis-water.webp",
+  },
+  {
+    id: "calpis-rich",
+    name: "カルピス THE RICH 冬仕込み",
+    description:
+      "冬限定のまろやかな濃さ。乳原料をリッチに使用したカルピスに北海道産ミルクと、ミルクソースを加えました。一休みしたい時など、自分を甘やかしてあげたい時にぴったりな、冬限定のまろやかな濃さが楽しめます。",
+    price: 160,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/calpis-rich.webp",
+  },
+  {
+    id: "peach-calpis",
+    name: "フルボディピーチ&カルピス",
+    description:
+      "重みのある桃の濃さとカルピスの甘ずっぱさが織りなすおいしさ。まるでワインのテイスティングのようにひと口目にワクワクを感じつつ、ボディ感のある桃の濃さとカルピスのやさしさをお楽しみいただけます",
+    price: 160,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/peach-calpis.webp",
+  },
+  {
+    id: "coffee",
+    name: "ブラックコーヒー",
+    description:
+      "香りひろがる、心地よいコク。深煎り豆を丁寧に抽出した奥深い味わい。挽きたて豆と淹れたて時のような香りが楽しめるブラックコーヒーです。",
+    price: 140,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/coffee.webp",
+  },
+  {
+    id: "ayataka",
+    name: "綾鷹",
+    description:
+      "豊かなうまみとかろやかな後味。今の時代にあわせて、茶師と協働して仕上げた「まるで淹れたて一杯目のおいしさ」です。旨みはしっかり本格、なのに後味は軽やかな味わい。",
+    price: 130,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/ayataka.webp",
+  },
+  {
+    id: "fanta-orange",
+    name: "ファンタ オレンジ",
+    description:
+      "爽やかなオレンジ風味が楽しめる炭酸飲料。オレンジの風味がしっかりと感じられ、よりキリっとした軽やかな後味をお楽しみいただけます。子供から大人まで幅広く愛される定番の味わいです。",
+    price: 140,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/fanta-orange.webp",
+  },
+  {
+    id: "mugi",
+    name: "むぎ茶",
+    description:
+      "香ばしい麦の風味が特徴のむぎ茶。焙煎の異なる3種類の六条大麦を使用しています。カフェインレスなので小さなお子様や妊娠中の方でも安心してお飲み頂けます。",
+    price: 110,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/mugi.webp",
+  },
+  {
+    id: "black-oolong",
+    name: "黒烏龍茶",
+    description:
+      "脂肪の吸収を抑え、体に脂肪がつきにくくなる特定保健用食品のウーロン茶です。独自製法でカフェイン量を抑えた他、苦味も少なく、飲みやすい味わいで、食事によく合い、無理なく毎日飲み続けられるさっぱりとした後味に仕上げました。",
+    price: 160,
+    imageUrl: "https://r2.mogami.dev/miuye-zikken/black-oolong.webp",
+  },
+];
